27131ef1d3fba17094cb8ea09d8d27604d9785bf
shadypierre <shadypierre@gmail.com>
2013-02-08 15:06:13 +0100

    correction fonction on() (qui remplace live() ) pour jQuery 1.9

--- Resources/public/js/ajax.js
+++ Resources/public/js/ajax.js
@@ -1,58 +1,62 @@
 $(document).ready(function() {
-    $('body').prepend("<div id='canvasloader-container'></div>");
+    $('body').prepend('<div id="canvasloader-container"></div>');
     createLoader('#canvasloader-container');
-    $("form.ajax").on('submit',function(event) {
+    $(document).on('submit', 'form.ajax', function(event) {
         $('#canvasloader-container').fadeIn();
         event.preventDefault();
-        var update = $(this).attr("update")?$(this).attr("update"):"";
+        var update = $(this).attr('update')?$(this).attr('update') : '';
         var form = $(this);
-
         ajaxFormSubmit(form,update);
+        
         return false;
     });
-    $("a.ajax").on('click',function(event) {
+    
+    $(document).on('click', 'a.ajax', function(event) {
         $('#canvasloader-container').fadeIn();
         event.preventDefault();
-        var update = $(this).attr("update")?$(this).attr("update"):"";
-        var link = $(this).attr("href");
+        var update = $(this).attr('update') ? $(this).attr('update') : '';
+        var link   = $(this).attr('href');
         ajaxLink(link,update);
+        
         return false;
     });
 });
-function ajaxFormSubmit(form,update){
+
+function ajaxFormSubmit(form,update) {
     $.ajax({
         url: $(form).attr('action'),
         context: document.body,
         data: $(form).serialize(),
         type: $(form).attr('method'),
-        success: function(jsonResponse){
+        success: function(jsonResponse) {
             ajaxify(jsonResponse, update);
         }
     });
 }
 
-function ajaxLink(link,update){
+function ajaxLink(link,update) {
     $.ajax({
         url: link,
         context: document.body,
         type: "GET",
-        success: function(jsonResponse){
+        success: function(jsonResponse) {
             ajaxify(jsonResponse, update);
         },
-        error: function(jsonResponse){
+        error: function(jsonResponse) {
             alert("Il semble s'êre produit une erreur");
             $('#canvasloader-container').fadeOut();
         }
     });
 }
 
-function ajaxify(jsonResponse, update){
-    if(jsonResponse.substring(0,1)=="{" && jsonResponse.substring(jsonResponse.length-1)=="}"){
+function ajaxify(jsonResponse, update) {
+    if (jsonResponse.substring(0,1)=="{" && jsonResponse.substring(jsonResponse.length-1)=="}") {
         var json = eval("("+jsonResponse+")");
-        if(json.update != undefined)
+        if (json.update != undefined) {
             update = json.update;
+        }
         // check if a callback is given in the response
-        if(json.hasOwnProperty("callback")){
+        if (json.hasOwnProperty("callback")) {
             $.post(json.callback, 
             {
                 params : json.data
@@ -60,10 +64,10 @@ function ajaxify(jsonResponse, update){
             function(data){
                 $("#"+update).html(data);
             });
-        }else{//if no callback has been given, we put data in the element to update
+        } else {//if no callback has been given, we put data in the element to update
             $("#"+update).html(json.data);
         }
-    }else{
+    } else {
         $("#"+update).html(jsonResponse);
     }
     
@@ -71,7 +75,6 @@ function ajaxify(jsonResponse, update){
 
 }
 
-function createLoader(id){
+function createLoader(id) {
     $(id).html("<img src='/bundles/avawesomealertify/images/ajax-loader.gif' title='Chargement en cours' alt='Chargement' width='32' height='32'/>");
 }
-
