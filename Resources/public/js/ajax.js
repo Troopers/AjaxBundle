ee93761702f68fcda07e9d87b8be4ea4a5f7e41c
lenybernard <leny@appventus.com>
2013-02-27 17:32:01 +0100

    add effect support

--- Resources/public/js/ajax.js
+++ Resources/public/js/ajax.js
@@ -50,11 +50,13 @@ function ajaxLink(link,update) {
 }
 
 function ajaxify(jsonResponse, update) {
+    var effect;
     if (jsonResponse.substring(0,1)=="{" && jsonResponse.substring(jsonResponse.length-1)=="}") {
         var json = eval("("+jsonResponse+")");
         if (json.update != undefined) {
             update = json.update;
         }
+        effect = initEffect("#"+update);
         // check if a callback is given in the response
         if (json.hasOwnProperty("callback")) {
             $.post(json.callback,
@@ -68,13 +70,37 @@ function ajaxify(jsonResponse, update) {
             $("#"+update).html(json.data);
         }
     } else {
+        effect = initEffect("#"+update);
         $("#"+update).html(jsonResponse);
+    }
+    if(effect != undefined){
+        eval('$("#"+update).'+effect+'()');
     }
 
     $('#canvasloader-container').fadeOut();
 
 }
 
+function initEffect(id) {
+    var effect = "show";
+    if($(id).html() != undefined){
+        if($(id).html() == ""){
+            effect = "slideDown";
+            if($(id).attr('data-new-effect') != undefined && $(id).attr('data-new-effect') != ""){
+                effect = $(id).attr('data-new-effect');
+            }
+        }else{
+            effect = "fadeIn";
+            if($(id).attr('data-update-effect') != undefined && $(id).attr('data-update-effect') != ""){
+                effect = $(id).attr('data-update-effect');
+            }
+        }
+    }
+    $(id).hide();
+    console.log(effect);
+
+    return effect;
+}
 function createLoader(id) {
     $(id).html("<img src='/bundles/avajax/images/ajax-loader.gif' title='Chargement en cours' alt='Chargement' width='32' height='32'/>");
 }
