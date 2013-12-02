dfe85e49ccf2b570a53582112401d77155b2a186
Paul Andrieux <paul@appventus.com>
2013-12-02 11:07:26 +0100

    add custom modal class support

--- Resources/public/js/ajax-modal.js
+++ Resources/public/js/ajax-modal.js
@@ -6,11 +6,12 @@ $(document).ready(function() {
     $('[data-toggle="modal"]').click(function(e) {
         e.preventDefault();
         var url = $(this).attr('href');
+        var customClass = $(this).attr('data-modal-class') ? $(this).attr('data-modal-class') : '';
         if (url.indexOf('#') == 0) {
             $(url).modal('open');
         } else {
             $.get(url, function(data) {
-                $('<div class="modal hide fade">' + data + '</div>').modal();
+                $('<div class="modal hide fade ' + customClass + '">' + data + '</div>').modal();
             }).success(function() { $('input:text:visible:first').focus(); });
         }
     });
