44d75cf3225450a1c5b8ecd67ce88e5a93e1aa4f
lenybernard <leny@appventus.com>
2014-03-10 23:44:54 +0100

    Refactor effect + add no-effect attribute to specify no effect

--- Resources/public/js/ajax.js
+++ Resources/public/js/ajax.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
         var update = $(this).data('update')?$(this).data('update') : $(this).attr('data-target') ? $(this).attr('data-target') : '';
         var updateStrategy = $(this).data('updateStrategy') ? $(this).data('updateStrategy') : 'html';
         var form = $(this);
-        ajaxFormSubmit(form, $(form).attr('action'), update, updateStrategy);
+        var effect = guessEffect(this, "#" + update);
+        ajaxFormSubmit(form, $(form).attr('action'), update, updateStrategy, effect);
 
         return false;
     });
@@ -21,45 +22,46 @@ $(document).ready(function() {
         }
         $('#canvasloader-container').fadeIn();
         event.preventDefault();
-        
+
         //is the link linked ot a form
         var formSelector = $(this).data('form');
-        
+
         var update = $(this).data('update') ? $(this).data('update') : $(this).attr('data-target') ? $(this).attr('data-target') : '';
         var updateStrategy = $(this).data('updateStrategy') ? $(this).data('updateStrategy') : 'html';
         var link   = $(this).attr('href');
-        
+        var effect = guessEffect(this, "#" + update);
+
         //if there is a form we submit this one with the href of the link
         if (formSelector === undefined) {
             ajaxLink(link, update, updateStrategy);
         } else {
             var form = $(formSelector);
-            ajaxFormSubmit(form, link, update, updateStrategy);
+            ajaxFormSubmit(form, link, update, updateStrategy, effect);
         }
 
         return false;
     });
 });
 
-function ajaxFormSubmit(form, action, update, updateStrategy) {
+function ajaxFormSubmit(form, action, update, updateStrategy, effect) {
     $.ajax({
-        url: action,
-        context: document.body,
-        data: $(form).serialize(),
-        type: $(form).attr('method'),
-        success: function(jsonResponse) {
-            ajaxify(jsonResponse, update, updateStrategy);
+        url     : action,
+        context : document.body,
+        data    : $(form).serialize(),
+        type    : $(form).attr('method'),
+        success : function(jsonResponse) {
+            ajaxify(jsonResponse, update, updateStrategy, effect);
         }
     });
 }
 
-function ajaxLink(link,update, updateStrategy) {
+function ajaxLink(link,update, updateStrategy, effect) {
     $.ajax({
-        url: link,
-        context: document.body,
-        type: "GET",
-        success: function(jsonResponse) {
-            ajaxify(jsonResponse, update, updateStrategy);
+        url     : link,
+        context : document.body,
+        type    : "GET",
+        success : function(jsonResponse) {
+            ajaxify(jsonResponse, update, updateStrategy, effect);
         },
         error: function(jsonResponse) {
             if (typeof toastr === 'undefined') {
@@ -75,29 +77,37 @@ function ajaxLink(link,update, updateStrategy) {
     });
 }
 
-function ajaxify(jsonResponse, update, updateStrategy) {
-    var effect;
+function ajaxify(jsonResponse, update, updateStrategy, effect) {
 
     if (typeof jsonResponse === 'object') {
         handleJson(jsonResponse, update, updateStrategy);
     } else {
+        if (effect != undefined && effect != null){
+            $("#" + update).hide();
+        }
         //By default, the updateStrategy is html (a simple replace) but you can set your own function
         //for example, append, after etc or even a custom one.
-        eval('$("#"+update).' + updateStrategy + '(jsonResponse)');
-        effect = guessEffect("#"+update);
-        eval('$("#"+update).'+effect+'()');
+        eval('$("#" + update).' + updateStrategy + '(jsonResponse)');
+
+        if (effect != undefined && effect != null){
+            eval('$("#" + update).' + effect + '()');
+        }
     }
 
     $('#canvasloader-container').fadeOut();
 
 }
 
-function handleJson(json, update, updateStrategy) {
+function handleJson(json, update, updateStrategy, effect) {
 
     if (json.hasOwnProperty("update")) {
         update = json.update;
     }
-    effect = guessEffect("#"+update);
+
+    if (effect != undefined && effect != null){
+        $("#" + update).hide();
+    }
+
     // check if an ajax callback is given in the response, execute it
     if (json.hasOwnProperty("ajax-callback")) {
         $.post(json.callback,
@@ -105,7 +115,7 @@ function handleJson(json, update, updateStrategy) {
             params : json.data
         },
         function(data){
-            eval('$("#"+update).' + updateStrategy + '(data)');
+            eval('$("#" + update).' + updateStrategy + '(data)');
         });
     }
     // a callback is javascript code
@@ -114,9 +124,10 @@ function handleJson(json, update, updateStrategy) {
     }
     // html is the html part to be inserted in the "update"
     if (json.hasOwnProperty("html")) {
-        $("#"+update).html(json.html);
-        if(effect != undefined){
-            eval('$("#"+update).'+effect+'()');
+        eval('$("#" + update).' + updateStrategy + '(json.html)');
+
+        if (effect != undefined && effect != null){
+            eval('$("#" + update).'+effect+'()');
         }
     }
     // redirect is an url
@@ -125,26 +136,31 @@ function handleJson(json, update, updateStrategy) {
     }
 }
 
-function guessEffect(id) {
+function guessEffect(link, targetId) {
     var effect = "show";
-    if ($(id).html() != undefined) {
-        if ($(id).html() == "") {
+    if ($(targetId).html() != undefined) {
+        if ($(targetId).html() == "") {
             effect = "slideDown";
-            if ($(id).attr('data-new-effect') != undefined && $(id).attr('data-new-effect') != "") {
-                effect = $(id).attr('data-new-effect');
+            if ($(link).attr('data-new-effect') != undefined && $(link).attr('data-new-effect') != "") {
+                effect = $(link).attr('data-new-effect');
+            } else if ($(targetId).attr('data-new-effect') != undefined && $(targetId).attr('data-new-effect') != "") {
+                effect = $(targetId).attr('data-new-effect');
             }
         } else {
             effect = "fadeIn";
-            if ($(id).attr('data-update-effect') != undefined && $(id).attr('data-update-effect') != "") {
-                effect = $(id).attr('data-update-effect');
+            if ($(link).attr('data-update-effect') != undefined && $(link).attr('data-update-effect') != "") {
+                effect = $(link).attr('data-update-effect');
+            } else if ($(targetId).attr('data-update-effect') != undefined && $(targetId).attr('data-update-effect') != "") {
+                effect = $(targetId).attr('data-update-effect');
             }
         }
+    } else if ($(link).data('no-effect')) {
+        effect = null;
     }
-
-    $(id).hide();
 
     return effect;
 }
+
 function createLoader(id) {
     $(id).html("<img src='/bundles/avajax/images/ajax-loader.gif' title='Chargement en cours' alt='Chargement' width='32' height='32'/>");
 }
