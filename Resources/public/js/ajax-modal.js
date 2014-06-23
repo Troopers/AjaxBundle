76c83ef8cad71e75a6063e408f293b909ffde1d3
Paul Andrieux <paul@appventus.com>
2014-06-23 18:43:04 +0200

    do not hide modal

--- Resources/public/js/ajax-modal.js
+++ Resources/public/js/ajax-modal.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
             $(url).modal('open');
         } else {
             $.get(url, function(data) {
-                $('<div class="modal hide fade ' + customClass + '">' + data + '</div>').modal();
+                $('<div class="modal ' + customClass + '">' + data + '</div>').modal();
             }).success(function() { $('input:text:visible:first').focus(); });
         }
     });
